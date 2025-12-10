0ebc4fbce861780c1d944222e411d1c9361c4727
GrishaVEVO <ggrishaben@gmail.com>
2025-12-11 02:42:51 +0300

    с_1A

--- level1.py
+++ level1.py
@@ -1,2 +1,52 @@
-print()
-print ("Hello")
+"""a = int(input())
+b = int(input())
+print(b // a)""" #1А Груши
+
+"""a = int(input())
+b = int(input())
+print(b % a)""" #1А Тоже груши
+
+"""a = int(input())
+print(a % 10)""" #1А Последняя цифра
+
+"""a = int(input())
+print(a // 10)""" #1А Количество десятков двузначного числа
+
+"""a = int(input())
+print ( a // 10 % 10)""" #1А Количество десятков
+
+"""a = int(input())
+print(a // 100 + a // 10 % 10 + a % 10 % 10)""" #1А Сумма цифр
+
+"""m = int(input())
+m = m % (24 * 60)
+print(m // 60, m % 60)""" #1А Электронные часы
+
+"""a = int(input())
+b = int(input())
+k = int(input())
+total_a = a * k
+total_b = b * k
+print(total_a + total_b // 100, total_b % 100)""" #1А Стоимость булочек
+
+"""h1 = int(input())
+m1 = int(input())
+s1 = int(input())
+h2 = int(input())
+m2 = int(input())
+s2 = int(input())
+tot_sec1 = h1 * 3600 + m1 * 60 + s1
+tot_sec2 = h2 * 3600 + m2 * 60 + s2
+delta = tot_sec2 - tot_sec1
+print(delta)""" #1А Разность времён
+
+"""import math
+a = int(input())
+b = int(input())
+print(math.sqrt (a**2 + b**2))""" #1А Длина гипотенузы
+
+"""a = int(input())
+print("The next number for the number ", a, " is ", a + 1, ".", sep="")
+print(f"The next number for the number {a} is {a + 1}.")
+print(f"The previous number for the number {a} is {a - 1}.")""" #1А После и перед (sep и f-строку)
+
